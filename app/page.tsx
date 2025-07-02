988ae525863290eff4b604a06118c3f1e4091451
Aditya <adityabasak7031@gmail.com>
2025-07-02 18:30:29 +0530

    add commponents to the page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { Navbar } from "@/components/Navbar";
+import { About } from "@/components/About";
+import { Hero } from "@/components/Hero";
+import { Services } from "@/components/Services";
+import { SubAbout } from "@/components/Sub-about";
+import serviceData from "@/data/services.json"
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <Navbar />
+    <main className="min-h-screen dark:bg-gray-950">
+
+      <Hero />
+      <About />
+      <SubAbout />
+      <Services items={serviceData} />
 
     </main>
   )
